021026785ae89b0a33eee7d566c147823a7bfb23
Arg0z <97903291+Arg0z@users.noreply.github.com>
2024-10-19 16:22:18 -0400

    Updated controller

--- GameController.cpp
+++ GameController.cpp
@@ -8,6 +8,7 @@
 
 GameController::GameController()
 {
+	state = LEVEL1;
 	m_sdlEvent = { };
 }
 
@@ -17,100 +18,6 @@ GameController::~GameController()
 
 void GameController::RunGame()
 {
-	// Level 1 logic
-	/*Level1* level1 = new Level1();
-	Renderer* r = &Renderer::Instance();
-	Timing* t = &Timing::Instance();
-	
-
-	TTFont* font = new TTFont();
-	font->Initialize(20);
-
-
-	SpriteSheet::Pool = new ObjectPool<SpriteSheet>();
-	SpriteAnim::Pool = new ObjectPool<SpriteAnim>();
-	SpriteSheet* sheetWarrior = SpriteSheet::Pool->GetResource();
-	sheetWarrior->Load("Assets/Textures/Warrior.tga");
-	sheetWarrior->SetSize(17, 6, 69, 44);
-	sheetWarrior->AddAnimation(EN_AN_IDLE, 0, 6, 2.0f);
-	sheetWarrior->AddAnimation(EN_AN_RUN, 6, 8, 2.0f);
-
-	SpriteSheet* sheetRock = SpriteSheet::Pool->GetResource();
-	sheetRock->Load("Assets/Textures/Rock.tga");
-	sheetRock->SetSize(1, 4, 20, 20);
-	sheetRock->AddAnimation(EN_AN_IDLE, 0, 1, 6.0f);
-
-	
-	level1->AssignNonDefaultValues();
-	level1->startLevel(sheetWarrior, nullptr);
-
-	while (!level1->isFinished())
-	{
-		t->Tick();
-		SDL_PollEvent(&m_sdlEvent);
-		if (m_sdlEvent.type == SDL_QUIT)
-			break;
-
-		level1->Update(font);
-		
-		SDL_RenderPresent(r->GetRenderer());
-	}
-
-	delete SpriteAnim::Pool;
-	delete SpriteSheet::Pool;
-	delete level1;
-
-	font->Shutdown();
-	r->Shutdown();*/
-
-	// Level 2 logic
-	/*Level2* level2 = new Level2();
-	Renderer* r2 = &Renderer::Instance();
-	Timing* t2 = &Timing::Instance();
-
-
-	TTFont* font2 = new TTFont();
-	font2->Initialize(20);
-
-
-	SpriteSheet::Pool = new ObjectPool<SpriteSheet>();
-	SpriteAnim::Pool = new ObjectPool<SpriteAnim>();
-	SpriteSheet* sheetWarrior2 = SpriteSheet::Pool->GetResource();
-	sheetWarrior2->Load("Assets/Textures/Warrior.tga");
-	sheetWarrior2->SetSize(17, 6, 69, 44);
-	sheetWarrior2->AddAnimation(EN_AN_IDLE, 0, 6, 2.0f);
-	sheetWarrior2->AddAnimation(EN_AN_RUN, 6, 8, 2.0f);
-	sheetWarrior2->AddAnimation(EN_AN_DEATH, 26, 11, 2.0f);
-
-	SpriteSheet* sheetRock2 = SpriteSheet::Pool->GetResource();
-	sheetRock2->Load("Assets/Textures/Rock.tga");
-	sheetRock2->SetSize(1, 4, 20, 20);
-	sheetRock2->AddAnimation(EN_AN_IDLE, 0, 0, 6.0f);
-
-
-	level2->AssignNonDefaultValues();
-	level2->startLevel(sheetWarrior2, sheetRock2);
-
-	while (!level2->isFinished())
-	{
-		t2->Tick();
-		SDL_PollEvent(&m_sdlEvent);
-		if (m_sdlEvent.type == SDL_QUIT)
-			break;
-
-		level2->Update(font2);
-
-		SDL_RenderPresent(r2->GetRenderer());
-	}
-
-	delete SpriteAnim::Pool;
-	delete SpriteSheet::Pool;
-	delete level2;
-
-	font2->Shutdown();
-	r2->Shutdown();*/
-
-	// TEMP LOGIC
 	Level1* level1 = new Level1();
 	Level2* level2 = new Level2();
 	level1->AssignNonDefaultValues();
@@ -142,21 +49,33 @@ void GameController::RunGame()
 	level1->startLevel(sheetWarrior, nullptr);
 	level2->startLevel(sheetWarrior, sheetRock);
 
-	while (true && !level2->isFinished())
+	while (state != GameState::QUIT)
 	{
 		t->Tick();
-
 		SDL_PollEvent(&m_sdlEvent);
 		if (m_sdlEvent.type == SDL_QUIT)
-			break;
+			state = QUIT;
 
-		if (!level1->isFinished())
+		switch (state)
 		{
-			level1->Update(font);
-		}
-		else
-		{
-			level2->Update(font);
+		case GameState::LEVEL1:
+			{
+				level1->Update(font);
+				if (level1->isFinished())
+				{
+					state = LEVEL2;
+				}
+				break;
+			}
+		case GameState::LEVEL2:
+			{
+				level2->Update(font);
+				if (level2->isFinished())
+				{
+					state = QUIT;
+				}
+				break;
+			}
 		}
 
 		SDL_RenderPresent(r->GetRenderer());
